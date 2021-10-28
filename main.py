d63ab073aa48eacbe019aae9112cbbf18143f8be
Abdelrahman Adel <adel65004@gmail.com>
2021-10-28 22:37:04 +0200

    Refactoring for my project

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+main_path = os.path.abspath(__file__[:-7])
+print(main_path)
+sys.path.insert(0, main_path + "/Display/")
+sys.path.insert(0, main_path + "/Processing/")
+
 import Ncurses_Display
 import System
+
 def main() :
   S = System.system
   Ncurses_Display.Display(S , 10)  
